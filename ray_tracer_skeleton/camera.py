cd1dce797a69d6d986e69a28b8d7ffe9814ba6a0
MichalBKM <berkheim1@mail.tau.ac.il>
2025-12-04 19:04:29 +0200

    Added setup_camera_axes to camera class

--- ray_tracer_skeleton/camera.py
+++ ray_tracer_skeleton/camera.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Camera:
     def __init__(self, position, look_at, up_vector, screen_distance, screen_width):
         self.position = position
@@ -5,5 +7,18 @@ class Camera:
         self.up_vector = up_vector
         self.screen_distance = screen_distance
         self.screen_width = screen_width
+        self.setop_camera_axes()
+    
+    def setup_camera_axes(self):
+        # FORWARD direction: calculation + normalization
+        self.forward = (self.look_at - self.position)
+        self.forward = self.forward / np.linalg(self.forward)
 
-#just checking git
+        # RIGHT direction: calculation + normalization
+        self.right = np.cross(self.forward, self.up_vector)
+        self.right = self.right / np.linalg(self.right)
+
+        # UP direction: calculation + normalization
+        self.up = np.cross(self.right, self.forward)
+        self.up = self.up / np.linalg.norm(self.up)
+
