09317d525128ca21a647532fe882ee95313ef1be
avivbaror <avivbaror@mail.tau.ac.il>
2025-12-09 14:58:06 +0200

    I NEED A CAREER CHANGE

--- ray_tracer_skeleton/surfaces/infinite_plane.py
+++ ray_tracer_skeleton/surfaces/infinite_plane.py
@@ -1,5 +1,16 @@
+import numpy as np
+
 class InfinitePlane:
     def __init__(self, normal, offset, material_index):
         self.normal = normal
         self.offset = offset
         self.material_index = material_index
+    
+    def intersection(self, ray):
+        # t = -(P0 • N + d) / (V • N)
+        try:
+            t = -(np.dot(self.normal, ray.origin) + self.offset) / (np.dot(self.normal, ray.direction))
+        except ZeroDivisionError:
+            return None
+        p = ray.origin + t * ray.direction
+        return p
--- ray_tracer_skeleton/surfaces/sphere.py
+++ ray_tracer_skeleton/surfaces/sphere.py
@@ -1,5 +1,28 @@
+import numpy as np
+
 class Sphere:
     def __init__(self, position, radius, material_index):
         self.position = position
         self.radius = radius
         self.material_index = material_index
+
+    def intersection(self, ray):
+        L = self.position - ray.origin
+        tca = np.dot(L, ray.direction)
+        if tca < 0:
+            return None
+        distance_squared = np.dot(L, L) - tca**2
+        if distance_squared > self.radius**2:
+            return None
+        thc = np.sqrt(self.radius**2 - distance_squared)
+        t0 = tca - thc
+        t1 = tca + thc
+        if t0 > 0:
+            t = t0
+        elif t1 > 0:
+            t = t1
+        else:
+            return None
+        P = ray.origin + t * ray.direction
+        N = (P - self.position) / np.linalg.norm(P - self.position)
+        return t, P, N
